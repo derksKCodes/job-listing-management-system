6fd2088debf0b4a76ceea0b6e114795a5f904fa5
derksKCodes <derksthetribal.ghub@gmail.com>
2025-07-13 13:57:36 +0300

    tailwindcss initialization and project setup configs

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// @type {import('tailwindcss').Config}
+export default{
+    content:[
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./index.html"
+    ],
+    theme: {
+        extend: {
+            // colors: {
+            //     primary: '#1DA1F2',
+            //     secondary: '#14171A',
+            //     accent: '#657786',
+            // },
+            // fontFamily: {
+            //     sans: ['Inter', 'sans-serif'],
+            //     serif: ['Merriweather', 'serif'],
+            // },
+        },
+        plugins: [],
+    },
+}
